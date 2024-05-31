40d955e16e9d0978ab82eedc5f5e3af581162f71
Deesubainsla <deesubainsla340@gmail.com>
2024-05-31 21:58:24 +0530

    Adding few components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authservice from './appwrite/auth';
+import { login, logout } from './store/authSlice';
 
 function App() {
  
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+      authservice.getCurrentUser()
+      .then((userData)=>{
+
+          if(userData){
+              dispatch(login({userData}));
+          }else{
+              dispatch(logout());
+          }
+
+      })
+      .finally(()=> setLoading(false))
+  }, [])
+
+
   return (
     <>
-     <h1>hello appwrite</h1>
+     <h1 className='bg-red-700 text-white'>hello appwrite</h1>
     </>
   )
 }
