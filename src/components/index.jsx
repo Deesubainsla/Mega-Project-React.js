5d232f842387737d758803da8f18d24fb7087bdf
Deesubainsla <deesubainsla340@gmail.com>
2024-06-04 00:52:08 +0530

    Adding till signup component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./Container/Container";
 import Logoutbtn from "./Logoutbtn";
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import Select from "./Select"
 
-export {Header, Footer, Logo, Container, Logoutbtn}
+export {Header, Footer, Logo, Container, Logoutbtn, Button, Input, PostCard, Select }
